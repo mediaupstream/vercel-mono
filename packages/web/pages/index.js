8a30a9017faf1e27e7b791e360ea402c97531fc1
Derek <derek@mediaupstream.com>
2021-03-10 00:30:53 -0600

    fix: api url

--- packages/web/pages/index.js
+++ packages/web/pages/index.js
@@ -1,7 +1,9 @@
 import { Button, Box } from 'shared'
 import fetch from 'node-fetch'
+const API_URL = process.env.API_URL || 'http://localhost:3001'
 
 export default function Home({ data }) {
+  console.log(process.env.VERCEL_URL)
   return (
     <div style={{ padding: "2rem"}}>
       <Button>Shared Button</Button>
@@ -19,7 +21,7 @@ export default function Home({ data }) {
 }
 
 export async function getStaticProps(context) {
-  const data = await (await fetch('http://localhost:3001')).json()
+  const data = await (await fetch(API_URL)).json()
   return {
     props: { data },
   }
