4119fb8a5f339f17e29fac7db07fc6b6fbac17cb
Derek <derek@mediaupstream.com>
2021-03-17 12:03:30 -0500

    feat: better color

--- packages/shared/src/components/Box.js
+++ packages/shared/src/components/Box.js
@@ -5,7 +5,7 @@ import { fragments } from 'api/lib'
 console.log('from shared/Box', fragments)
 
 export const Box = styled.div`
-  background:green;
+  background: rebeccapurple;
   color:#fff;
   padding: 2rem;
   margin:1rem;
