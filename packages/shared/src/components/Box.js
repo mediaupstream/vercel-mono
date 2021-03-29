90de5211b9b2d8b41e54394bf6480a9f46ed1a4f
Derek <derek@mediaupstream.com>
2021-03-29 16:56:51 -0500

    feat: green box default

--- packages/shared/src/components/Box.js
+++ packages/shared/src/components/Box.js
@@ -5,7 +5,7 @@ import { fragments } from 'api/lib'
 console.log('from shared/Box', fragments)
 
 export const Box = styled.div`
-  background: pink;
+  background: green;
   color:#fff;
   padding: 2rem;
   margin:1rem;
