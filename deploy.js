f6e62f2085ead01e6663429ddb8b00bc9f8eb99f
Derek <derek@mediaupstream.com>
2021-03-29 21:42:02 -0500

    feat: better deploy script

--- deploy.js
+++ deploy.js
@@ -8,9 +8,6 @@ const TOKEN = 'VpcyGrjb0uOvrXsxGiyhxWIe'
 const isProd = process.env.PROD_RELEASE == 1
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 
-console.log(process.env)
-process.exit(1)
-
 if (!GITHUB_TOKEN) {
   console.error('Missing environment variable GITHUB_TOKEN')
   process.exit(1)
@@ -20,12 +17,6 @@ const octokit = new Octokit({
   auth: GITHUB_TOKEN,
   previews: ['ant-man-preview', 'flash-preview'],
 })
-
-const github = {
-  owner: process.env.GITHUB_OWNER || '',
-  repo: process.env.GITHUB_REPO || '',
-  environment: isProd ? 'production' : 'qa'
-}
 
 const projects = {
   api: 'prj_LQmlubkLgUq9jNtGmAiS6Qp0Llyv',
@@ -37,47 +28,94 @@ const withEnv = n => Object.entries(n).map(([k,v]) => `-b ${k}="${v}" -e ${k}="$
 
 const log = (...msg) => DEBUG ? console.log(...msg) : null
 
-async function createDeployment() {
-  // const out = await octokit.repos.createDeployment({
-  //   owner:
-  // })
-}
 
-async function deployApi() {
-  log('[ Deploying API ]')
-  const { stdout: res } = await exec(cmd(projects.api))
-  const url = res.toString().trim()
-  log('preview:', url, '\n')
-  return url
-}
-
-async function deployWeb(apiUrl) {
-  log('[ Deploying WEB ]')
-  const env = {
-    API_URL: apiUrl
+async function createDeployment(description, options = {}) {
+  try {
+    const defaults = {
+      owner: process.env.GH_OWNER_NAME || '',
+      repo: process.env.GH_REPO_NAME || '',
+      environment: process.env.DEPLOYMENT_ENV || (isProd ? 'production' : 'preview'),
+      ref: process.env.GITHUB_SHA,
+      transient_environment: !isProd,
+      production_environment: isProd,
+      auto_merge: true
+    }
+    const { data } = await octokit.repos.createDeployment({
+      ...defaults,
+      description,
+      ...options
+    })
+    // try again?
+    if (!data.id && data.message) {
+      return createDeployment(description, options)
+    }
+    console.log(data)
+    return data.id
+  } catch (err) {
+    throw new Error('Cannot create deployment:', err)
   }
-  const { stdout: res } = await exec(`${cmd(projects.web)} ${withEnv(env)}`)
-  const url = res.toString().trim()
-  log('preview:', url, '\n')
-  return url
 }
 
-async function deploy() {
+// state: error, failure, pending, in_progress, queued, success
+async function updateDeployment(deployment_id, state = 'in_progress', environment_url) {
+  try {
+    const options = {
+      owner: process.env.GH_OWNER_NAME || '',
+      repo: process.env.GH_REPO_NAME || '',
+      auto_inactive: false,
+      deployment_id,
+      state,
+      environment_url
+    }
+    await octokit.repos.createDeploymentStatus(options)
+  } catch (err) {
+    throw new Error('Cannot update deployment:', err)
+  }
+}
+
+function serviceDeploy(service, fn) {
+  return async (...args) => {
+    try {
+      log('+ - - - - - - - - - - - - - - - - ')
+      log(`+ Creating ${service} deployment`)
+      const deploymentId = await createDeployment(`Create ${service} deployment`)
+
+      log('> Updating deployment status: in_progress')
+      await updateDeployment(deploymentId)
+
+      log(`> Deploying ${service}`)
+      const url = await fn(...args)
+
+      log('> Deployment URL:', url)
+
+      log('> Updating deployment status: success')
+      await updateDeployment(deploymentId, 'success', url)
+
+      return url
+    } catch (err) {
+      await updateDeployment(deploymentId, 'error')
+      throw err
+    }
+  }
+}
+
+const deployApi = serviceDeploy('API', async () => {
+  const { stdout: res } = await exec(cmd(projects.api))
+  return res.toString().trim()
+})
+
+const deployWeb = serviceDeploy('WEB', async (apiUrl) => {
+  const { stdout: res } = await exec(`${cmd(projects.web)} ${withEnv(env)}`)
+  return res.toString().trim()
+})
+
+
+(async function () {
   try {
     const apiUrl = await deployApi()
     const webUrl = await deployWeb(apiUrl)
-    const msg = [
-      ':tada: **Previews Deployed** :tada:',
-      '<ul>',
-      `<li>**API** - ${apiUrl}</li>`,
-      `<li>**WEB** - ${webUrl}</li>`,
-      '</ul>'
-    ]
-    console.log(msg.join(''))
   } catch (err) {
-    console.log('[ FAIL ]', err)
+    console.log('Deployment failed', err)
     process.exit(1)
   }
-}
-
-deploy()
+})()
