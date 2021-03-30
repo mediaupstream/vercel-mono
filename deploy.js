fad6edaeae439e9f5bdf1116aa82f19a7460ac9a
Derek <derek@mediaupstream.com>
2021-03-29 20:24:21 -0500

    feat: test deployments

--- deploy.js
+++ deploy.js
@@ -1,11 +1,31 @@
 const { promisify } = require('util')
+import { Octokit } from '@octokit/rest'
 const exec = promisify(require('child_process').exec)
-
-const isProd = process.env.PROD_RELEASE == 1
 
 const DEBUG = false
 const ORG_ID = 's0u8XySVq3EH7yrs1IRNb3Hj'
 const TOKEN = 'VpcyGrjb0uOvrXsxGiyhxWIe'
+const isProd = process.env.PROD_RELEASE == 1
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+
+console.log(process.env)
+process.exit(1)
+
+if (!GITHUB_TOKEN) {
+  console.error('Missing environment variable GITHUB_TOKEN')
+  process.exit(1)
+}
+
+const octokit = new Octokit({
+  auth: GITHUB_TOKEN,
+  previews: ['ant-man-preview', 'flash-preview'],
+})
+
+const github = {
+  owner: process.env.GITHUB_OWNER || '',
+  repo: process.env.GITHUB_REPO || '',
+  environment: isProd ? 'production' : 'qa'
+}
 
 const projects = {
   api: 'prj_LQmlubkLgUq9jNtGmAiS6Qp0Llyv',
@@ -16,6 +36,12 @@ const cmd = id => `VERCEL_ORG_ID=${ORG_ID} VERCEL_PROJECT_ID=${id} npx vercel --
 const withEnv = n => Object.entries(n).map(([k,v]) => `-b ${k}="${v}" -e ${k}="${v}"`).join(' ')
 
 const log = (...msg) => DEBUG ? console.log(...msg) : null
+
+async function createDeployment() {
+  // const out = await octokit.repos.createDeployment({
+  //   owner:
+  // })
+}
 
 async function deployApi() {
   log('[ Deploying API ]')
