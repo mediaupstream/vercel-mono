323da6b3229f53dd6a388f41dfda2fec49f7d832
Derek <derek@mediaupstream.com>
2021-04-01 14:21:14 -0500

    feat: use node script instead of shell

--- deploy.js
+++ deploy.js
@@ -3,10 +3,25 @@ const { Octokit } = require('@octokit/rest')
 const exec = promisify(require('child_process').exec)
 
 const DEBUG = true
+const PROJECTS = {
+  api: 'prj_LQmlubkLgUq9jNtGmAiS6Qp0Llyv',
+  web: 'prj_1ov6CWEJ2FOEAdMmJAvrk4QQjHWx',
+}
 const ORG_ID = 's0u8XySVq3EH7yrs1IRNb3Hj'
 const TOKEN = 'VpcyGrjb0uOvrXsxGiyhxWIe'
-const isProd = process.env.PROD_RELEASE == 1
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+const {
+  PROD_RELEASE,
+  GIT_BRANCH,
+  GITHUB_TOKEN,
+  GITHUB_SHA,
+  GITHUB_ORG_NAME,
+  GITHUB_REPO,
+  COMMIT_AUTHOR,
+  GIT_COMMIT_MSG,
+  COMMIT_AUTHOR_NAME
+} = process.env
+
+const isProd = PROD_RELEASE == 1
 
 if (!GITHUB_TOKEN) {
   console.error('Missing environment variable GITHUB_TOKEN')
@@ -18,55 +33,74 @@ const octokit = new Octokit({
   previews: ['ant-man-preview', 'flash-preview'],
 })
 
-const projects = {
-  api: 'prj_LQmlubkLgUq9jNtGmAiS6Qp0Llyv',
-  web: 'prj_1ov6CWEJ2FOEAdMmJAvrk4QQjHWx',
-}
 
-const cmd = id => `VERCEL_ORG_ID=${ORG_ID} VERCEL_PROJECT_ID=${id} npx vercel --token ${TOKEN} -c -C ${isProd ? '--prod' : ''}`
-const withEnv = n => Object.entries(n).map(([k,v]) => `-b ${k}="${v}" -e ${k}="${v}"`).join(' ')
+/**
+ * Vercel command to deploy project by `id`
+ */
+const cmd = project_id => `VERCEL_ORG_ID=${ORG_ID} VERCEL_PROJECT_ID=${project_id} npx vercel --token ${TOKEN} -c -C ${isProd ? '--prod' : ''}`
 
+/**
+ * Generate env/build flags for the `vercel` cmd
+ */
+const withEnv = n => Object.entries(n || {}).map(([k,v]) => `-b ${k}="${v}" -e ${k}="${v}"`).join(' ')
+
+/**
+ * Generate meta flags for the `vercel` cmd
+ */
+const withMeta = n => Object.entries(n || {}).map(([k,v]) => `-m ${k}="${v}"`).join(' ')
+
+/**
+ * Log stuff
+ */
 const log = (...msg) => DEBUG ? console.log(...msg) : null
 
-
-async function createDeployment(description, options = {}) {
+/**
+ * Creates a Github Deployment for the given environment
+ */
+async function createDeployment(environment) {
   try {
-    const defaults = {
-      owner: process.env.GH_OWNER_NAME || '',
-      repo: process.env.GH_REPO_NAME || '',
-      environment: process.env.DEPLOYMENT_ENV || (isProd ? 'production' : 'preview'),
-      ref: process.env.GITHUB_REF,
+    const options = {
+      environment,
+      owner: GITHUB_ORG_NAME,
+      repo: GITHUB_REPO,
+      ref: GITHUB_SHA,
       transient_environment: !isProd,
       production_environment: isProd,
-      auto_merge: true
+      auto_merge: false
     }
-    const { data } = await octokit.repos.createDeployment({
-      ...defaults,
-      description,
-      ...options
-    })
+    const { data } = await octokit.repos.createDeployment(options)
     // try again?
     if (!data.id && data.message) {
-      return createDeployment(description, options)
+      return createDeployment(environment)
     }
     log(data)
     return data.id
   } catch (err) {
     log(err)
-    throw new Error('Cannot create deployment:', err)
+    throw new Error('Error creating Github Deployment:', err)
   }
 }
 
-// state: error, failure, pending, in_progress, queued, success
-async function updateDeployment(deployment_id, state = 'in_progress', environment_url) {
+/**
+ * Update a Github Deployment status
+ */
+async function updateDeployment({
+  deployment_id,
+  state = 'success',
+  url
+}) {
+  if (!deployment_id) {
+    throw new Error('Missing deployment_id in updateDeployment')
+  }
   try {
     const options = {
-      owner: process.env.GH_OWNER_NAME || '',
-      repo: process.env.GH_REPO_NAME || '',
+      owner: GITHUB_ORG_NAME,
+      repo: GITHUB_REPO,
       auto_inactive: false,
       deployment_id,
       state,
-      environment_url
+      environment_url: url,
+      target_url: url
     }
     const { data } = await octokit.repos.createDeploymentStatus(options)
     log(data)
@@ -76,42 +110,75 @@ async function updateDeployment(deployment_id, state = 'in_progress', environmen
   }
 }
 
-function serviceDeploy(service, fn) {
+/**
+ * Creates an environment specific deployment fn
+ */
+function serviceDeploy(environment, fn) {
   return async (...args) => {
+    let deployment_id;
     try {
-      log('+ - - - - - - - - - - - - - - - - ')
-      log(`+ Creating ${service} deployment`)
-      const deploymentId = await createDeployment(`Create ${service} deployment`)
-
-      log('> Updating deployment status: in_progress')
-      await updateDeployment(deploymentId)
-
-      log(`> Deploying ${service}`)
+      deployment_id = await createDeployment(
+        `${environment} - ${isProd ? 'Production' : 'Preview'}`
+      )
       const url = await fn(...args)
-
-      log('> Deployment URL:', url)
-
-      log('> Updating deployment status: success')
-      await updateDeployment(deploymentId, 'success', url)
-
+      await updateDeployment({
+        deployment_id,
+        state: 'success',
+        url
+      })
       return url
     } catch (err) {
       log(err)
-      await updateDeployment(deploymentId, 'error')
+      if (deployment_id) {
+        await updateDeployment({
+          deployment_id,
+          state: 'failure'
+        })
+      }
       throw err
     }
   }
 }
 
+const vercelDeploy = ({ project_id, env, meta: metaOptions }) => {
+  if (!project_id) {
+    throw new Error('Missing `project_id` in vercelDeploy')
+  }
+  const meta = {
+    // magic meta keys to simulate the vercel github integration
+    githubCommitSha: GITHUB_SHA,
+    githubDeployment: 1,
+    githubOrg: GITHUB_ORG_NAME,
+    githubCommitOrg: GITHUB_ORG_NAME,
+    githubRepo: GITHUB_REPO,
+    githubCommitRepo: GITHUB_REPO,
+    githubCommitRef: GIT_BRANCH,
+    githubCommitAuthorLogin: COMMIT_AUTHOR,
+    githubCommitMessage: GIT_COMMIT_MSG,
+    githubCommitAuthorName: COMMIT_AUTHOR_NAME,
+    ...metaOptions
+  }
+  const command = `${cmd(project_id)} ${withEnv(env)} ${withMeta(meta)}`
+  return await exec(command)
+}
+
 const deployApi = serviceDeploy('API', async () => {
-  const { stdout: res } = await exec(cmd(projects.api))
+  const { stdout: res } = await vercelDeploy({
+    project_id: PROJECTS.api
+  })
   return res.toString().trim()
 })
 
 const deployWeb = serviceDeploy('WEB', async (API_URL) => {
-  const { stdout: res } = await exec(`${cmd(projects.web)} ${withEnv({ API_URL })}`)
+  const { stdout: res } = await vercelDeploy({
+    project_id: PROJECTS.web,
+    env: {
+      API_URL
+    }
+  })
   return res.toString().trim()
 })
+
 
 
 async function main () {
