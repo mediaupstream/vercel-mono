2202c164c291c88616b7615543093e9f53d7a272
Derek <derek@mediaupstream.com>
2021-04-01 21:53:20 -0500

    feat: adds some color, larger font

--- packages/shared/src/components/Box.js
+++ packages/shared/src/components/Box.js
@@ -5,8 +5,9 @@ import { fragments } from 'api/lib'
 console.log('from shared/Box', fragments)
 
 export const Box = styled.div`
+  font-size:1.4rem;
   background: rgb(238,174,202);
-  background: radial-gradient(circle, rgba(238,174,202,0.4) 0%, rgba(148,187,233,0.4) 100%);
+  background: radial-gradient(circle, rgba(138,174,202,0.4) 0%, rgba(248,187,233,0.4) 100%);
   color:#333;
   border-radius: 0.5rem;
   padding: 2rem;
